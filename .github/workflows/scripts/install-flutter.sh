0904c4051c97ec90c446284e1d626543718a153c
Mike Diarmid <mike.diarmid@gmail.com>
2021-10-22 14:25:02 +0100

    ci: install flutter into github workspace and not repository root

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -2,11 +2,11 @@
 
 BRANCH=$1
 
-cd "$HOME" || exit 1
+cd "$GITHUB_WORKSPACE" || exit 1
 git clone https://github.com/flutter/flutter.git --depth 1 -b "$BRANCH" _flutter
 {
-  echo "$HOME/_flutter/bin";
   echo "$HOME/.pub-cache/bin";
-  echo "$HOME/_flutter/.pub-cache/bin";
-  echo "$HOME/_flutter/bin/cache/dart-sdk/bin";
+  echo "$GITHUB_WORKSPACE/_flutter/bin";
+  echo "$GITHUB_WORKSPACE/_flutter/.pub-cache/bin";
+  echo "$GITHUB_WORKSPACE/_flutter/bin/cache/dart-sdk/bin";
 } >> "$GITHUB_PATH"
