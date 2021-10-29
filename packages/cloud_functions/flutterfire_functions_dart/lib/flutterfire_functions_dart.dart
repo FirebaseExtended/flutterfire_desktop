38c7277a60e01d088192f710efbeefa55e1afb92
Tim Whiting <tim@whitings.org>
2021-10-29 10:56:52 -0600

    start of dart client

--- packages/cloud_functions/flutterfire_functions_dart/lib/flutterfire_functions_dart.dart
+++ packages/cloud_functions/flutterfire_functions_dart/lib/flutterfire_functions_dart.dart
@@ -1,4 +1,15 @@
+/// Support for Firebase authentication methods
+/// with pure dart implmentation.
+///
+library flutterfire_functions_dart;
+
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutterfire_core_dart/flutterfire_core_dart.dart';
+import 'package:http/http.dart' as http;
+part 'src/api.dart';
+
 class FirebaseCloudFunctions {
-  FirebaseApp app;
+  final FirebaseApp app;
   FirebaseCloudFunctions._({required this.app});
 }
