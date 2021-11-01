2a75f65e55fef010f2609d53f387b790fda78090
Tim Whiting <tim@whitings.org>
2021-11-01 11:11:59 -0600

    get example working on linux

--- packages/cloud_functions/cloud_functions_desktop/example/lib/main.dart
+++ packages/cloud_functions/cloud_functions_desktop/example/lib/main.dart
@@ -1,53 +1,54 @@
-// ignore_for_file: public_member_api_docs
+// ignore_for_file: require_trailing_commas
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:developer';
+import 'dart:core';
+
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core_desktop/firebase_core_desktop.dart'
+    as core_desktop;
+import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
+import 'package:cloud_functions_desktop/cloud_functions_desktop.dart';
+import 'package:cloud_functions_platform_interface/cloud_functions_platform_interface.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  String get name => 'foo';
-
-  FirebaseOptions get firebaseOptions => const FirebaseOptions(
-        appId: '1:448618578101:ios:0b650370bb29e29cac3efc',
-        apiKey: 'AIzaSyAgUhHU8wSJgO5MVNy95tMT07NEjzMOfz0',
-        projectId: 'react-native-firebase-testing',
-        messagingSenderId: '448618578101',
-      );
-
-  Future<void> initializeDefault() async {
-    final app = await Firebase.initializeApp(options: firebaseOptions);
-    log('Initialized default app $app');
-  }
-
-  Future<void> initializeSecondary() async {
-    final app = await Firebase.initializeApp(
-      name: name,
-      options: firebaseOptions,
+FirebaseOptions get firebaseOptions => const FirebaseOptions(
+      appId: '1:448618578101:ios:0b650370bb29e29cac3efc',
+      apiKey: 'AIzaSyAgUhHU8wSJgO5MVNy95tMT07NEjzMOfz0',
+      projectId: 'react-native-firebase-testing',
+      messagingSenderId: '448618578101',
     );
+late FirebaseFunctions functions;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final core = core_desktop.FirebaseCore();
+  FirebasePlatform.instance = core;
+  // ignore: invalid_use_of_visible_for_testing_member
+  Firebase.delegatePackingProperty = core;
 
-    log('Initialized $app');
-  }
+  final app =
+      await Firebase.initializeApp(name: 'app', options: firebaseOptions);
+  FirebaseFunctionsPlatform.instance = FirebaseFunctionsDesktop(app: app);
+  functions = FirebaseFunctions.instanceFor(app: app)
+    ..useFunctionsEmulator('localhost', 5001);
+  runApp(MyApp());
+}
 
-  void apps() {
-    final apps = Firebase.apps;
-    log('Currently initialized apps: $apps');
-  }
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  void options() {
-    final app = Firebase.app(name);
-    final options = app.options;
-    log('Current options for app $name: $options');
-  }
+class _MyAppState extends State<MyApp> {
+  List fruit = [];
 
-  Future<void> delete() async {
-    final app = Firebase.app(name);
-    await app.delete();
-    log('App $name deleted');
+  @override
+  void initState() {
+    super.initState();
   }
 
   @override
@@ -55,35 +56,42 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Firebase Core example app'),
+          title: const Text('Firebase Functions Example'),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: initializeDefault,
-                child: const Text('Initialize default app'),
-              ),
-              ElevatedButton(
-                onPressed: initializeSecondary,
-                child: const Text('Initialize secondary app'),
-              ),
-              ElevatedButton(
-                onPressed: apps,
-                child: const Text('Get apps'),
-              ),
-              ElevatedButton(
-                onPressed: options,
-                child: const Text('List options'),
-              ),
-              ElevatedButton(
-                onPressed: delete,
-                child: const Text('Delete app'),
-              ),
-            ],
+        body: Center(
+            child: ListView.builder(
+                itemCount: fruit.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('${fruit[index]}'),
+                  );
+                })),
+        floatingActionButton: Builder(
+          builder: (context) => FloatingActionButton.extended(
+            onPressed: () async {
+              // See index.js in the functions folder for the example function we
+              // are using for this example
+              final HttpsCallable callable = functions.httpsCallable(
+                  'listFruit',
+                  options: HttpsCallableOptions(
+                      timeout: const Duration(seconds: 5)));
+
+              await callable().then((v) {
+                setState(() {
+                  fruit.clear();
+                  v.data.forEach((f) {
+                    fruit.add(f);
+                  });
+                });
+              }).catchError((e) {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text('ERROR: $e'),
+                ));
+              });
+            },
+            label: const Text('Call Function'),
+            icon: const Icon(Icons.cloud),
+            backgroundColor: Colors.deepOrange,
           ),
         ),
       ),
