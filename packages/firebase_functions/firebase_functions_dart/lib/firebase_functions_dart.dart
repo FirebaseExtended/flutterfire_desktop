a83c5a7ae4b5eaa515e679d3f9b417a62876dbe9
Nils Reichardt <nils@reichardt.io>
2022-08-01 08:08:59 +0200

    chore: fix typo "implmentation" (#99)

--- packages/firebase_functions/firebase_functions_dart/lib/firebase_functions_dart.dart
+++ packages/firebase_functions/firebase_functions_dart/lib/firebase_functions_dart.dart
@@ -3,7 +3,7 @@
 // that can be found in the LICENSE file.
 
 /// Support for Firebase authentication methods
-/// with pure dart implmentation.
+/// with pure dart implementation.
 ///
 library flutterfire_functions_dart;
 
