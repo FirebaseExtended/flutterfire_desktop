00463de1ad010e06264f3423f7204b3aa72e76ad
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2023-02-26 16:24:36 +0100

    fix exports

--- packages/firebase_storage/firebase_storage_dart/lib/firebase_storage_dart.dart
+++ packages/firebase_storage/firebase_storage_dart/lib/firebase_storage_dart.dart
@@ -1,29 +1,13 @@
-library firebase_storage_dart;
-
-import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
-import 'dart:math';
-import 'dart:typed_data';
-
-import 'package:firebase_core_dart/firebase_core_dart.dart';
-import 'package:firebase_core_dart/ipc.dart';
-import 'package:http/http.dart' as http;
-import 'package:mime/mime.dart';
-import 'package:path/path.dart';
-
-part 'src/internal/source.dart';
-part 'src/internal/errors.dart';
-part 'src/internal/storage_multipart_request_builder.dart';
-part 'src/internal/http_client.dart';
-part 'src/internal/storage_api_client.dart';
-part 'src/full_metadata.dart';
-part 'src/settable_metadata.dart';
-part 'src/list_result.dart';
-part 'src/list_options.dart';
-part 'src/task_state.dart';
-part 'src/task_snapshot.dart';
-part 'src/task.dart';
-part 'src/put_string_format.dart';
-part 'src/reference.dart';
-part 'src/firebase_storage.dart';
+export 'src/lib.dart'
+    show
+        FirebaseStorage,
+        Reference,
+        Task,
+        TaskSnapshot,
+        ListResult,
+        ListOptions,
+        FirebaseStorageException,
+        FullMetadata,
+        SettableMetadata,
+        TaskState,
+        PutStringFormat;
---
+++ packages/firebase_storage/firebase_storage_dart/lib/src/lib.dart
@@ -0,0 +1,29 @@
+library firebase_storage_dart;
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+import 'dart:typed_data';
+
+import 'package:firebase_core_dart/firebase_core_dart.dart';
+import 'package:firebase_core_dart/ipc.dart';
+import 'package:http/http.dart' as http;
+import 'package:mime/mime.dart';
+import 'package:path/path.dart';
+
+part 'internal/source.dart';
+part 'internal/errors.dart';
+part 'internal/storage_multipart_request_builder.dart';
+part 'internal/http_client.dart';
+part 'internal/storage_api_client.dart';
+part 'full_metadata.dart';
+part 'settable_metadata.dart';
+part 'list_result.dart';
+part 'list_options.dart';
+part 'task_state.dart';
+part 'task_snapshot.dart';
+part 'task.dart';
+part 'put_string_format.dart';
+part 'reference.dart';
+part 'firebase_storage.dart';
